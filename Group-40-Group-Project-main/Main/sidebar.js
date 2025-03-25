14fb3cd9d956e0d62291bb5b52ccb51b62c443fc
HarryM05 <149320410+HarryM05@users.noreply.github.com>
2025-03-25 02:57:20 +0000

    Completed Homepages +MORE
    Added: SettingsPersonal, SettingsSecurity, ContactUs, AboutUs
    Added JavaScript to change the colour of button's referring to the active page.
    Edited templates to incorporate JavaScript as an example on how to use it.
    Edited CSS to not center text for some headings - Added text centering on individual pages (listed above) for more flexibility

---
+++ Group-40-Group-Project-main/Main/sidebar.js
@@ -0,0 +1,31 @@
+//Changes the colour of the button reffering to the active page
+function addActiveClassToSidebarLinks() {
+    //Gets the end of the URL, which is the name of the active page
+    const currentUrl = window.location.pathname.split('/').pop();
+    const sidebarLinks = document.querySelectorAll('.sidebar-button a');
+
+    //Loops through links of all sidebar buttons
+    sidebarLinks.forEach(sidebarLink => {
+        const linkHref = sidebarLink.getAttribute('href');
+
+        //Compares sidebar button link to active page link
+        if (currentUrl === linkHref) {
+            //Adds active label so we can refer to the active button in css
+            sidebarLink.classList.add('active');
+        }
+    });
+}
+
+//Prevents page refreshing when the button reffering to the active page is clicked
+function preventPageRefresh(event) {
+    const links = document.querySelectorAll('.stayOnPageLink');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            history.pushState({}, '', window.location.href);
+        });
+    });
+}
+//Runs function when all Document Object Model's have loaded
+document.addEventListener('DOMContentLoaded', addActiveClassToSidebarLinks);
