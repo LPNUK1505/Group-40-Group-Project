846099e75b5a1336d3c2980e7b7dd8147b1469d3
HarryM05 <149320410+HarryM05@users.noreply.github.com>
2025-03-26 15:41:13 +0000

    Remove Burgermenu
    Removed Burger Menu from all pages because we are no longer having a responsive sidebar

---
+++ example.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hello World!</p>
+    <p>This is an example of how to commit an update on a new branch!</p>
+    <p>Group Project meeting today, make sure you attend!</p>
+    <p>hello you!</p>
+</body>
+</html>
