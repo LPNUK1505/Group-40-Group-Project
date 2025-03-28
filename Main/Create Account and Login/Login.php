17d14fd17edcd7efe333b85fdbc4344e807b814e
Muhammad <muhammadyusufhassan9@gmail.com>
2025-03-28 16:02:01 +0000

    moving branch info to new branch

---
+++ Main/Create Account and Login/Login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+   <!-- JAVA line for 'fontawesome' icons -->
+   <script src="https://kit.fontawesome.com/d15bb23cbb.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="../styles.css">
+
+
+   <!-- Creates the Header at the top -->
+   <div class="header">
+       <div class="header-menu-icon">
+           <i class="fa-solid fa-bars"></i>
+       </div>
+
+       <!-- Creates icons for the right hand side of the header -->
+       <div class="header-right-icon">
+            <a href="SettingsPersonal.html">
+                <i class="fa-solid fa-gear"></i> 
+            </a>
+            <i class="fa-solid fa-user-large"></i>
+       </div>
+   </div>
+   
+
+   <!-- Creates the Sidebar on the left hand side -->
+   <div class="sidebar">
+       <img src="../GoIkonLogoFinal.png" alt="Goikon Logo" class = "goikon-logo">
+
+   </div>
+
+
+   <!-- Creates the Footer at the bottom -->
+   <div class="footer">
+
+       <!-- Creates buttons in the footer -->
+       <a href="AboutUs.html" class="stayOnPageLink">About Us</a>
+       <a href="ContactUs.html">Contact Us</a>
+   </div>
+   
+   <div class="main-content">
+    <h2>Login</h2>
+    <form method="post" action="Login.php" id="login-form">
+
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+       
+        <button type="submit">Login</button> 
+        <p><a href="CreateAccount.php" class="create-account">Create Account</a></p>
+
+    </form>
+    </div>
+</html>
